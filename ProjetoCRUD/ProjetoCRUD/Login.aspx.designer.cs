b82977b2e91369d98e2d11f521c3f4e31ec8c8d4
Joao victor <jvictor_oliveira@outlook.com.br>
2019-10-31 22:39:41 -0300

    Inserindo a Tela de Login

--- ProjetoCRUD/ProjetoCRUD/Login.aspx.designer.cs
+++ ProjetoCRUD/ProjetoCRUD/Login.aspx.designer.cs
@@ -10,7 +10,7 @@
 namespace ProjetoCRUD {
     
     
-    public partial class frmCliente {
+    public partial class Login {
         
         /// <summary>
         /// Controle form1.
@@ -20,5 +20,14 @@ namespace ProjetoCRUD {
         /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Controle btnLogar.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnLogar;
     }
 }
